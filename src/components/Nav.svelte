4892516596af05a764254a346be3f4ac3ffe2ae2
JDoel-stannp <150908036+JDoel-stannp@users.noreply.github.com>
2025-01-31 14:03:43 +0000

    moved files into repo

---
+++ src/components/Nav.svelte
@@ -0,0 +1,6 @@
+<script>
+
+</script>
+
+<p>red test</p>
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import Nav from '../components/Nav.svelte';
+    import Footer from '../components/Footer.svelte';
+    import "../app.postcss";
+</script>
+
+<Nav class=""/>
+
+<Footer id=""/>
+
+<style>
+   
+</style>
