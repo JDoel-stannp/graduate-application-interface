a63f1c55738a2c860f325ea83a9c8fb6788ee5ac
Luke Ellis <contact@lukeellis.me>
2025-02-04 20:16:33 +0000

    Routes

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GraduateController;
+
+Route::get('/test', function () {
+    return response()->json(['message' => 'Test route is working!']);
+});
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Disable CSRF for routes in this group by removing the VerifyCsrfToken middleware.
+Route::prefix('api')
+    ->withoutMiddleware([\Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class])
+    ->group(function () {
+        Route::apiResource('graduates', GraduateController::class);
+    });
